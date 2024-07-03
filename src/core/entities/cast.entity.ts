7da2851cb053faafefcedc2cd15537acdd4b41e9
delafuentej <jfuentelucas@gmail.com>
2024-07-03 16:08:43 +0200

    data collection & data structure: actors movies

---
+++ src/core/entities/cast.entity.ts
@@ -0,0 +1,8 @@
+/* eslint-disable eol-last */
+/* eslint-disable prettier/prettier */
+export interface Cast {
+    id: number;
+    name: string;
+    character: string;
+    avatar: string;
+}
---
+++ src/core/use-cases/movie/get-cast.use-case.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+import { HttpAdapter } from '../../../config/adapters/http/http.adapter';
+import { MovieDBCastResponse } from '../../../infrastructure/interfaces/movie-db.responses';
+import { CastMapper } from '../../../infrastructure/mappers/cast.mapper';
+import { Cast } from '../../entities/cast.entity';
+
+
+export const getMovieCastUseCase = async(
+    fetcher: HttpAdapter,
+    movieId: number,
+): Promise<Cast[]>=>{
+        try{
+            const {cast} = await fetcher.get<MovieDBCastResponse>(`/${movieId}/credits`);
+            const actors = cast.map( actor => CastMapper.fromMovieDBCastToEntity(actor));
+            return actors;
+        }catch(error){
+            console.log(error);
+            throw new Error(`Cannot get movie cast: ${movieId}`);
+        }
+};
